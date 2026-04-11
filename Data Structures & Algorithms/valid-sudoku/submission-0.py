0f7aeb51cd37a14b2261d3c641c5385663813e68
Ang Koon Hian <33430416+angkoonhian@users.noreply.github.com>
2026-04-11 15:48:26 +0800

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -0,0 +1,38 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        for i in range(len(board)):
+            # Check if there are vertical duplicates
+            vertical_map = {}
+            for row in board:
+                if row[i] != ".":
+                    if row[i] in vertical_map:
+                        return False
+                    vertical_map[row[i]] = 1
+
+            # Check if there are horizontal duplicates - done within rows, refresh within rows
+            horizontal_map = {}
+            # populate maps
+            for num in board[i]:
+                if num != ".":
+                    if num in horizontal_map:
+                        return False
+                    horizontal_map[num] = 1
+
+            # Check grid duplicates
+            grid_map = {}
+            curr_horizontal_index = i // 3
+            for j in range(3):
+                for x in range(3):
+                    final_horizontal_index = curr_horizontal_index * 3 + x 
+                    num = board[j][final_horizontal_index]
+                    print(f"{num}, {j}, {final_horizontal_index}")
+                    if num != ".":
+                        if num in grid_map:
+                            print("HERE")
+                            # print(f"{num}, {curr_vertical_index}, {final_horizontal_index}")
+                            return False
+                        grid_map[num] = 1
+
+        return True
+        
